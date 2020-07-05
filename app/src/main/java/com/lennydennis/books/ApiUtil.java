100b061c3487439f7c383f06c2da55b3dac8853c
LennyDennis <lennydennis@gmail.com>
2020-07-05 12:11:20 +0300

    - Add a URI Builder class for creating the api urls reference - Add and API key to use for the GoogleBooks API

--- app/src/main/java/com/lennydennis/books/ApiUtil.java
+++ app/src/main/java/com/lennydennis/books/ApiUtil.java
@@ -1,5 +1,6 @@
 package com.lennydennis.books;
 
+import android.net.Uri;
 import android.util.Log;
 
 import java.io.IOException;
@@ -9,17 +10,25 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class ApiUtil {
+    private static final String QUERY_PARAMETER_KEY = "q";
+
     private ApiUtil(){}
 
     public static final String BASE_API_URL =
             "https://www.googleapis.com/books/v1/volumes";
+    public static final String KEY = "key";
+    public static  final String API_KEY = "AIzaSyC9GkzI70tFTJP6KZGE9G998mSeVbS7F_U";
+
 
     public static URL buildUrl(String title){
-        String fullUrl = BASE_API_URL+"?q="+title;
 
         URL url = null;
+        Uri uri = Uri.parse(BASE_API_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAMETER_KEY, title)
+                .appendQueryParameter(KEY,API_KEY)
+                .build();
         try {
-            url = new URL(fullUrl);
+            url = new URL(uri.toString());
         }
         catch (Exception e){
             e.printStackTrace();
--- app/src/main/java/com/lennydennis/books/MainActivity.java
+++ app/src/main/java/com/lennydennis/books/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -13,10 +15,13 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar mLoadingProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLoadingProgress = findViewById(R.id.pb_loading);
 
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
@@ -46,7 +51,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             TextView tvResult = findViewById(R.id.tv_response);
+            TextView tvError = findViewById(R.id.tv_error);
+            mLoadingProgress.setVisibility(View.INVISIBLE);
+            if(result == null){
+                tvResult.setVisibility(View.INVISIBLE);
+                tvError.setVisibility(View.VISIBLE);
+            }else{
+                tvResult.setVisibility(View.VISIBLE);
+                tvError.setVisibility(View.INVISIBLE);
+            }
             tvResult.setText(result);
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgress.setVisibility(View.VISIBLE);
         }
     }
 }
