8a3adf7b537634b361429cf7562474de0b6ab964
LennyDennis <lennydennis@gmail.com>
2020-07-05 11:10:51 +0300

    - Add a BooksQueryTask that allows the netwok call to the googlebooks API to happen in the background thread

--- app/src/main/java/com/lennydennis/books/MainActivity.java
+++ app/src/main/java/com/lennydennis/books/MainActivity.java
@@ -2,7 +2,14 @@ package com.lennydennis.books;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            URL bookUrl = ApiUtil.buildUrl("cooking");
+            new BooksQueryTask().execute(bookUrl);
+        }
+        catch (Exception e){
+            Log.d("error", e.getMessage());
+        }
+
+
+    }
+    public class BooksQueryTask extends AsyncTask<URL, Void, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String result = null;
+            try{
+                result = ApiUtil.getJson(searchUrl);
+            }
+            catch (Exception e){
+                Log.e("Error", e.getMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView tvResult = findViewById(R.id.tv_response);
+            tvResult.setText(result);
+        }
     }
 }
